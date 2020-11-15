5be68541648f568b32ab6893fa7ab368afad466b
Ali Haghani <alihaghani96@gmail.com>
2020-11-14 17:40:35 -0800

    Updated App component with search and load more functionality.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,68 @@
-import logo from './logo.svg';
 import './styles.css';
 
-function App() {
+import { useCallback, useEffect, useState } from 'react';
+
+import { getPhotos, searchPhotos } from '../../utils/unsplash_api';
+import PhotoGallery from '../PhotoGallery';
+import resultTypes from '../../constants/result_types';
+import SearchBar from '../../components/SearchBar';
+
+const App = () => {
+  const [photoResults, setPhotoResults] = useState([]);
+  const [resultsConfig, setResultsConfig] = useState({
+    type: resultTypes.LATEST,
+    lastFetchedPage: 1,
+  })
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const photosResponse = await getPhotos();
+      setPhotoResults(photosResponse);
+    }
+    fetchPhotos();
+  }, []);
+
+  const fetchSearchPhotos = useCallback(async (query) => {
+    const photosResponse = await searchPhotos(query);
+    setPhotoResults(photosResponse);
+    setResultsConfig({ type: resultTypes.SEARCH, query, lastFetchedPage: 1 });
+  }, []);
+
+  const fetchMorePhotos = async () => {
+    const newFetchedPage = resultsConfig.lastFetchedPage + 1;
+    if (resultsConfig.type === resultTypes.LATEST) {
+      const photosResponse = await getPhotos(newFetchedPage);
+      setPhotoResults([...photoResults, ...photosResponse]);
+      setResultsConfig({
+        type: resultTypes.LATEST,
+        lastFetchedPage: newFetchedPage,
+      });
+    }
+    if (resultsConfig.type === resultTypes.SEARCH) {
+      const photosResponse = await searchPhotos(resultsConfig.query, newFetchedPage);
+      // TODO: don't keep fetching if already at the last page
+      setPhotoResults([...photoResults, ...photosResponse]);
+      setResultsConfig({
+        ...resultsConfig,
+        type: resultTypes.SEARCH,
+        lastFetchedPage: newFetchedPage,
+      });
+    }
+  };
+
   return (
     <div className="App">
-
+      <SearchBar searchPhotos={fetchSearchPhotos} />
+      <PhotoGallery photos={photoResults} />
+      <button
+        className="load-more-button"
+        onClick={fetchMorePhotos}
+        type="submit"
+      >
+        Load 10 more photos...
+      </button>
     </div>
   );
-}
+};
 
 export default App;
