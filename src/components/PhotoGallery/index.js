6aa1974b737812b9d37d163c7f16bbfdefac190d
Ali Haghani <alihaghani96@gmail.com>
2020-11-14 17:38:51 -0800

    Photo gallery component and styling.

---
+++ src/components/PhotoGallery/index.js
@@ -0,0 +1,24 @@
+import './styles.css';
+
+const PhotoGallery = ({ photos }) => {
+
+  return (
+    <div className="photo-gallery-container">
+      {
+        photos.map(photo => (
+          <div key={photo.id} className="photo-container">
+            <a rel="noreferrer" target="_blank" href={photo.links.html}>
+              <img
+                alt={photo.alt_description}
+                className="gallery-image"
+                src={photo.urls.small}
+              />
+            </a>
+          </div>
+        ))
+      }
+    </div>
+  )
+};
+
+export default PhotoGallery;
