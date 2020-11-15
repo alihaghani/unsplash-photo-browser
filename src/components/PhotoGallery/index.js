dbbb526404e36d95ec108cbc1b0ad6eeed56fcac
Ali Haghani <alihaghani96@gmail.com>
2020-11-14 20:37:50 -0800

    Formatting.

--- src/components/PhotoGallery/index.js
+++ src/components/PhotoGallery/index.js
@@ -1,22 +1,21 @@
 import './styles.css';
 
-const PhotoGallery = ({ photos }) =>
-  (
-    <div className="photo-gallery-container">
-      {
-        photos.map(photo => (
-          <div key={photo.id} className="photo-container">
-            <a rel="noreferrer" target="_blank" href={photo.links.html}>
-              <img
-                alt={photo.alt_description}
-                className="gallery-image"
-                src={photo.urls.small}
-              />
-            </a>
-          </div>
-        ))
-      }
-    </div>
-  );
+const PhotoGallery = ({ photos }) => (
+  <div className="photo-gallery-container">
+    {
+      photos.map(photo => (
+        <div key={photo.id} className="photo-container">
+          <a rel="noreferrer" target="_blank" href={photo.links.html}>
+            <img
+              alt={photo.alt_description}
+              className="gallery-image"
+              src={photo.urls.small}
+            />
+          </a>
+        </div>
+      ))
+    }
+  </div>
+);
 
 export default PhotoGallery;
